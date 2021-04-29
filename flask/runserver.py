0e34806f475583c670208940dfc80f0206d0543a
Matthew Dawson <4986903+matthew-dawson@users.noreply.github.com>
2021-04-29 04:32:56 +0100

    Aws (#2)
    * Base VPC instantiation
    
    * ECS Task Execution Role creation
    
    * Application ECS Task Definition
    
    * Cloudwatch Log Group creation
    
    * Load balancer creation
    
    * Load balancer target group creation
    
    * Load balancer listener creation
    
    * Tidying up a few errors with the task definition and ECR
    
    * Updating Docker and Flask config to allow for db username/password passing via environment variables
    
    * Hacky update to app in order to support passing environment variables during instantiation of containers
    
    * Creation of the RDS Database instance
    
    * Updating ECS Task Definition to supply the appropriate environment variables to the instantiated container
    
    * ECS Service creation
    
    * Tidying up the CFT deployment mechanism

--- flask/runserver.py
+++ flask/runserver.py
@@ -1,8 +1,22 @@
 from notejam import app
 from notejam.config import DevelopmentConfig
+from notejam.config import ProductionConfig
+from notejam.config import TestingConfig
+import os
 
 #TODO: Determine environment based upon environment variable
 app.config.from_object(DevelopmentConfig)
 
 if __name__ == '__main__':
+    environment = os.getenv('ENVIRONMENT', 'TESTING')
+    sql_uri = os.getenv('SQL_URI')
+
+
+    if environment == 'development':
+        app.config.from_object(DevelopmentConfig)
+        app.config.SQLALCHEMY_DATABASE_URI = sql_uri
+    elif environment == 'production':
+        app.config.from_object(ProductionConfig)
+        app.config.SQLALCHEMY_DATABASE_URI = sql_uri
+
     app.run("0.0.0.0")
