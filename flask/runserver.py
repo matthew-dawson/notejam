c87459f10bbbe15c4ea5d5971eeeb1fa95a0a9a6
Matthew Dawson <matthew-dawson@users.noreply.github.com>
2021-05-07 19:46:44 +0100

    Addressing configuration issues within Flask which had broken the db integration

--- flask/runserver.py
+++ flask/runserver.py
@@ -6,14 +6,14 @@ import os
 
 if __name__ == '__main__':
     environment = os.getenv('ENVIRONMENT', 'TESTING')
-    sql_uri = os.getenv('SQL_URI')
-
 
     if environment == 'development':
+        print("Development environment, loading development config")
         app.config.from_object(DevelopmentConfig)
-        app.config.SQLALCHEMY_DATABASE_URI = sql_uri
+
     elif environment == 'production':
+        print("Production environment, loading production config")
         app.config.from_object(ProductionConfig)
-        app.config.SQLALCHEMY_DATABASE_URI = sql_uri
+
 
     app.run("0.0.0.0")
